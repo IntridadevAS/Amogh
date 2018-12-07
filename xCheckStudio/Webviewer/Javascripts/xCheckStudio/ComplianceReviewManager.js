f8b0c7813e68c383480235c0a52b0a3eef80d098
yuvrajnalawadepts <44164026+yuvrajnalawadepts@users.noreply.github.com>
2018-12-07 11:51:11 +0530

    Updated code to populate detailed review table for compliance check on component selection in main review table.

--- xCheckStudio/Webviewer/Javascripts/xCheckStudio/ComplianceReviewManager.js
+++ xCheckStudio/Webviewer/Javascripts/xCheckStudio/ComplianceReviewManager.js
@@ -1,10 +1,14 @@
 
-function ComplianceReviewManager(complianceCheckManager) {
+function ComplianceReviewManager(complianceCheckManager,
+                                 mainReviewTableContainer,
+                                 detailedReviewTableContainer) {
     this.ComplianceCheckManager = complianceCheckManager;
 
+    this.MainReviewTableContainer = mainReviewTableContainer;
+    this.DetailedReviewTableContainer = detailedReviewTableContainer;
 
-    ComplianceReviewManager.prototype.populateReviewTable = function (reviewTableContainer) {
-        var parentTable = document.getElementById(reviewTableContainer);
+    ComplianceReviewManager.prototype.populateReviewTable = function () {
+        var parentTable = document.getElementById( this.MainReviewTableContainer);
 
         for (var componentsGroupName in this.ComplianceCheckManager.CheckComponentsGroups) {
             var componentsGroup = this.ComplianceCheckManager.CheckComponentsGroups[componentsGroupName];
@@ -96,9 +100,9 @@ function ComplianceReviewManager(complianceCheckManager) {
                 var componentIdentifier = component.SourceAName;
 
                 if (componentsGroup.ComponentClass === "PipingNetworkSegment") {
-                    var checkPropertySource = component.getCheckProperty('Source', 'Source', false);
-                    var checkPropertyDestination = component.getCheckProperty('Destination', 'Destination', false);
-                    var checkPropertyOwnerId = component.getCheckProperty('OwnerId', 'OwnerId', false);
+                    var checkPropertySource = component.getCheckProperty('Source', '', true);
+                    var checkPropertyDestination = component.getCheckProperty('Destination', '', true);
+                    var checkPropertyOwnerId = component.getCheckProperty('OwnerId', '', true);
 
                     td = document.createElement("td");
                     if (checkPropertySource != undefined) {
@@ -221,6 +225,173 @@ function ComplianceReviewManager(complianceCheckManager) {
             currentRow.onmouseout = createMouseOutHandler(currentRow);
         }
     }
+
+    ComplianceReviewManager.prototype.populateDetailedReviewTable = function (row) {
+
+        var parentTable = document.getElementById(this.DetailedReviewTableContainer);
+        parentTable.innerHTML = '';
+
+        if (row.cells[this.MainReviewTableStatusCell].innerHTML.toLowerCase() === "no match") {
+
+
+            return;
+        }
+
+        var reviewTableId = this.getReviewTableId(row);
+
+        for (var componentsGroupName in this.ComplianceCheckManager.CheckComponentsGroups) {
+
+            // get the componentgroupd corresponding to selected component 
+            var componentsGroup = this.ComplianceCheckManager.CheckComponentsGroups[componentsGroupName];
+            if (componentsGroup.ComponentClass != reviewTableId) {
+                continue;
+            }
+
+            for (var i = 0; i < componentsGroup.Components.length; i++) {
+                var component = componentsGroup.Components[i];
+
+                if (component.Status.toLowerCase() === "no match") {
+                    continue;
+                }
+
+                var source1NameCell = row.getElementsByTagName("td")[0];
+                var source2NameCell = row.getElementsByTagName("td")[1];
+
+                if (component.SourceAName !== source1NameCell.innerHTML &&
+                    component.SourceBName !== source2NameCell.innerHTML) {
+                    continue;
+                }
+
+                // if component is PipingNetworkSegment, check if source and destination properties are same
+                // because they may have same tag names
+                if (componentsGroup.ComponentClass === "PipingNetworkSegment") {
+                    var checkPropertySource = component.getCheckProperty('Source', '', true);
+                    var checkPropertyDestination = component.getCheckProperty('Destination', '', true);
+                    var checkPropertyOwnerId = component.getCheckProperty('OwnerId', '', true);
+
+                    if (checkPropertySource != undefined &&
+                        checkPropertyDestination != undefined &&
+                        checkPropertyOwnerId != undefined) {
+
+                        var source = row.cells[this.MainReviewTableStatusCell + 1].innerHTML;
+                        var destination = row.cells[this.MainReviewTableStatusCell + 2].innerHTML;
+                        var ownerId = row.cells[this.MainReviewTableStatusCell + 3].innerHTML;
+
+                        if (checkPropertySource.SourceAValue !== source ||
+                            checkPropertyDestination.SourceAValue !== destination ||
+                            checkPropertyOwnerId.SourceAValue !== ownerId) {
+                            continue;
+                        }
+                    }
+                }
+
+                // var parentTable = document.getElementById("ComparisonDetailedReviewCell");
+                // parentTable.innerHTML = '';
+
+                var div = document.createElement("DIV");
+                parentTable.appendChild(div);
+
+                div.innerHTML = "Check Details :";
+                div.style.fontSize = "20px";
+                div.style.fontWeight = "bold";
+
+                var table = document.createElement("TABLE");
+                div.appendChild(table);
+
+                // thead
+                var thead = document.createElement("thead");
+                table.appendChild(thead);
+
+                var tr = document.createElement("tr");
+                thead.appendChild(tr);
+
+                var th = document.createElement("th");
+                th.innerHTML = "Source A Name"
+                tr.appendChild(th);
+
+                var th = document.createElement("th");
+                th.innerHTML = "Source A Value"
+                tr.appendChild(th);
+
+                // // if (!checkManager.ComplianceCheck) {
+                // th = document.createElement("th");
+                // th.innerHTML = "Source B Name"
+                // tr.appendChild(th);
+
+                // th = document.createElement("th");
+                // th.innerHTML = "Source B Value"
+                // tr.appendChild(th);
+                // // }
+
+                th = document.createElement("th");
+                th.innerHTML = "Status"
+                tr.appendChild(th);
+
+                var tbody = document.createElement("tbody");
+                table.appendChild(tbody);
+
+                // show component class name as property in detailed review table               
+                var property = new CheckProperty("ComponentClass",
+                    component.SubComponentClass,
+                    "ComponentClass",
+                    component.SubComponentClass,
+                    "",
+                    true,
+                    "");
+                tr = this.addPropertyRowToDetailedTable(property);
+                tbody.appendChild(tr);
+
+                for (var j = 0; j < component.CheckProperties.length; j++) {
+                    property = component.CheckProperties[j];
+
+                    tr = this.addPropertyRowToDetailedTable(property);
+                    tbody.appendChild(tr);
+                }
+
+                break;
+            }
+        }
+    }
+
+
+    ComplianceReviewManager.prototype.addPropertyRowToDetailedTable = function (property) {
+        var tr = document.createElement("tr");
+        //tbody.appendChild(tr);
+
+        var td = document.createElement("td");
+        td.innerHTML = property.SourceAName;
+        tr.appendChild(td);
+
+        td = document.createElement("td");
+        td.innerHTML = property.SourceAValue;
+        tr.appendChild(td);
+
+        // //if (!checkManager.ComplianceCheck) {
+        // td = document.createElement("td");
+        // td.innerHTML = property.SourceBName;
+        // tr.appendChild(td);
+
+        // td = document.createElement("td");
+        // td.innerHTML = property.SourceBValue;;
+        // tr.appendChild(td);
+        // //}
+
+        td = document.createElement("td");
+        if (property.PerformCheck &&
+            property.Result) {
+            td.innerHTML = "OK";
+        }
+        else {
+            td.innerHTML = property.Severity;
+        }
+        tr.appendChild(td);
+
+        // set row's background color according to status
+        tr.style.backgroundColor = this.getRowHighlightColor(td.innerHTML);
+
+        return tr;
+    }
+
 
     ComplianceReviewManager.prototype.ChangeBackgroundColor = function (row) {
         row.style.backgroundColor = "#9999ff";
