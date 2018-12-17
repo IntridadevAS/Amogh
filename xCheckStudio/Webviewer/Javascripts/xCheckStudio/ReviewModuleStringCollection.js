70644a526f7d6c0544bfde604a310ffbd34d5044
yuvrajnalawadepts <44164026+yuvrajnalawadepts@users.noreply.github.com>
2018-12-17 12:07:48 +0530

    Added missing file.

---
+++ xCheckStudio/Webviewer/Javascripts/xCheckStudio/ReviewModuleStringCollection.js
@@ -0,0 +1,4 @@
+var sourceAViewerContainerRowId = "sourceAContainerRow";
+var sourceBViewerContainerRowId = "sourceBContainerRow";
+var sourceAViewerContainerId = "viewerContainer1";
+var sourceBViewerContainerId = "viewerContainer2";
