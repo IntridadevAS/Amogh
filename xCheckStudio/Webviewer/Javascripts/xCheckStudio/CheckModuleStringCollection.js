0326a996065304907f902bcf774e678b95d66768
yuvrajnalawadepts <44164026+yuvrajnalawadepts@users.noreply.github.com>
2018-12-17 16:03:38 +0530

    Updated code to keep all Source A and Source B controls inactive at beginning. loadSource button for Source A will be enabled only when check case is selected. Other Source A controls will be active once Source A is loaded. loadSource button for Source B will become active, once Source A is loaded. After Source B is loaded, all source B controls will be active.

---
+++ xCheckStudio/Webviewer/Javascripts/xCheckStudio/CheckModuleStringCollection.js
@@ -0,0 +1,15 @@
+var sourceAFrontId = "Front1";
+var sourceABackId = "Back1";
+var sourceATopId = "Top1";
+var sourceABottomId = "Bottom1";
+var sourceALeftId = "Left1";
+var sourceARightId = "Right1";
+var sourceAIsoId = "Iso1";
+
+var sourceBFrontId = "Front2";
+var sourceBBackId = "Back2";
+var sourceBTopId = "Top2";
+var sourceBBottomId = "Bottom2";
+var sourceBLeftId = "Left2";
+var sourceBRightId = "Right2";
+var sourceBIsoId = "Iso2";
