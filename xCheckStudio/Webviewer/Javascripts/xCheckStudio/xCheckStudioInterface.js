d6394fd4dba05c948941942d2e0888e885c98bd5
Pooja Galande <pooja.galande@prototechsolutions.com>
2019-04-26 17:45:19 +0530

    Check all support for DB datasets

--- xCheckStudio/Webviewer/Javascripts/xCheckStudio/xCheckStudioInterface.js
+++ xCheckStudio/Webviewer/Javascripts/xCheckStudio/xCheckStudioInterface.js
@@ -407,8 +407,12 @@ var xCheckStudio;
                 return this._modelTree;
             }
             else if (this.excelReader !== undefined &&
-                this.excelReader.excelModelBrowser !== undefined) {
+                this.excelReader.excelModelBrowser !== undefined && this.excelReader.SourceType == "xls") {
                 return this.excelReader.excelModelBrowser;
+            }
+            else if (this.db_reader !== undefined &&
+                this.db_reader.dbmodelbrowser !== undefined && this.db_reader.SourceType == "ibd") {
+                return this.db_reader.dbmodelbrowser;
             }
 
             return undefined;
