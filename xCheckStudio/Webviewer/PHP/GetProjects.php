e06dd75da68d436b325b3b8310b8ebeb242e302b
Rahul Khande <>
2019-04-26 16:59:00 +0530

    Added service to get list of existing projects for provided 'userid'

--- xCheckStudio/Webviewer/PHP/GetProjects.php
+++ xCheckStudio/Webviewer/PHP/GetProjects.php
@@ -1,6 +1,11 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $userid = $_POST["userid"];
+    $userid = trim($_POST["userid"], " ");
+    if($userid == "")
+    {
+        echo "UserId cannot be empty";
+        return;
+    }
     try{
         $dbh = new PDO("sqlite:../Data/Main.db") or die("cannot open the database");
         $query =  "select * from Projects where userid=".$userid.";";      
