c3794a15c3dc0624a27f4ecd16a271617d23c99f
Rahul Khande <>
2019-08-07 17:23:15 +0530

    Updated path utility functions

--- xCheckStudio/Webviewer/PHP/Utility.php
+++ xCheckStudio/Webviewer/PHP/Utility.php
@@ -1,14 +1,40 @@
-<?php
+<?php    
+    $projectpath = getCheckDirectoryPath($projectname, $checkname);
+    echo $projectpath;
+    echo '\n';
+    $projectpath = getCheckDatabasePath($projectname, $checkname);
+    echo $projectpath;
+    echo '\n';
+    return;
+
+    function getProjectDirectoryPath($projectName)
+    {
+        $projectDirPath = "../Projects/".$projectName;
+        return $projectDirPath;
+    }
 
     function getProjectDatabasePath($projectName)
     {
-        $dbPath = "../Projects/".$projectName."/".$projectName.".db";
+
+        $dbPath = getProjectDirectoryPath($projectName)."/Project.db";
+        return $dbPath;
+    }
+
+    function getCheckDirectoryPath($projectName, $checkName)
+    {
+        $checkDirPath = getProjectDirectoryPath($projectName)."/CheckSpaces/".$checkName;
+        return $checkDirPath;
+    }
+
+    function getCheckDatabasePath($projectName, $checkName)
+    {
+        $dbPath = getCheckDirectoryPath($projectName, $checkName)."/".$checkName."_temp.db";
         return $dbPath;
     }
 
     function tableExists($dbh, $tableName)
     {
-        $results = $dbh->query("SELECT *FROM $tableName;");
+        $results = $dbh->query("SELECT * FROM $tableName;");
         if($results) 
         {
             //var_dump($results);
