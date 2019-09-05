5f5bf417718470068339838a40420f5753654747
Yuvraj Nalawade <yuvraj.nalawade@prototechsolutions.com>
2019-09-05 17:24:29 +0530

    Fixed pan for 3D issue. Also imprved the context menu.

--- xCheckStudio/Webviewer/Javascripts/xCheckStudio/CheckViewerContextMenu.js
+++ xCheckStudio/Webviewer/Javascripts/xCheckStudio/CheckViewerContextMenu.js
@@ -3,100 +3,252 @@ function CheckViewerContextMenu(webViewer) {
     ContextMenuManager.call(this);
 
     this.WebViewer = webViewer;
-    // this.ModelBrowser;
-
-    // //this.IsolatedNodes = [];
-    // this.IsolateManager;
 
     this.TranslucencyManager;
     this.ExplodeManager;
+    
+    this.NavCubeVisible = true;
 }
 
 // inherit from parent
 CheckViewerContextMenu.prototype = Object.create(ContextMenuManager.prototype);
 CheckViewerContextMenu.prototype.constructor = CheckViewerContextMenu;
 
+CheckViewerContextMenu.prototype.ShowMenu = function (x, y) {
+
+    var _this = this;
+
+    var contextMenuDiv = document.getElementById("contextMenu");
+    contextMenuDiv.innerHTML = "";
+
+    // 1st group
+    var itemGroup = document.createElement("ul");
+    itemGroup.id = "items";
+
+    var item = document.createElement("li");
+    item.id = "hide";
+    item.innerText = "Hide";
+    item.onclick =
+    function () {
+        _this.OnHideClicked();
+    }
+    itemGroup.appendChild(item);
+
+    item = document.createElement("li");
+    item.id = "isolate";
+    item.innerText = "Isolate";
+    item.onclick = function () {
+        _this.OnIsolateClicked();
+    }
+    itemGroup.appendChild(item);
+
+    item = document.createElement("li");
+    item.id = "showAll";
+    item.innerText = "Show All";
+    item.onclick = function () {
+        _this.OnShowAllClicked();
+    }
+    itemGroup.appendChild(item);
+
+    contextMenuDiv.appendChild(itemGroup);
+
+    item = document.createElement("hr");
+    contextMenuDiv.appendChild(item);
+
+    // 2nd group
+    itemGroup = document.createElement("ul");
+    itemGroup.id = "items";
+
+    if (!this.NavCubeVisible) {
+        item = document.createElement("li");
+        item.id = "showNavigationCube";
+        item.innerText = "Show Navigation Cube";
+        item.onclick = function () {
+            showNavigationCube(_this.WebViewer);
+            
+            _this.NavCubeVisible = true;
+        };
+        itemGroup.appendChild(item);
+    }
+    else {
+        item = document.createElement("li");
+        item.id = "hideNavigationCube";
+        item.innerText = "Hide Navigation Cube";
+        item.onclick = function () {
+            hideNavigationCube(_this.WebViewer);
+
+            _this.NavCubeVisible = false;
+        };
+        itemGroup.appendChild(item);
+    }
+    contextMenuDiv.appendChild(itemGroup);
+
+    item = document.createElement("hr");
+    contextMenuDiv.appendChild(item);
+
+    // 3rd group
+    itemGroup = document.createElement("ul");
+    itemGroup.id = "items";
+    if (!this.ExplodeManager) {
+        item = document.createElement("li");
+        item.id = "startExplode";
+        item.innerText = "Start Explode";
+        item.onclick =  function () {
+             _this.OnStartExplodeClicked();
+        }
+        itemGroup.appendChild(item);
+    }
+    else {
+        item = document.createElement("li");
+        item.id = "stopExplode";
+        item.innerText = "Stop Explode";
+        item.onclick =  function () {
+            _this.OnStopExplodeClicked();
+        }
+        itemGroup.appendChild(item);
+    }
+    contextMenuDiv.appendChild(itemGroup);
+
+    item = document.createElement("hr");
+    contextMenuDiv.appendChild(item);
+
+    // 4th group
+    itemGroup = document.createElement("ul");
+    itemGroup.id = "items";
+
+    if (!this.TranslucencyManager) {
+        item = document.createElement("li");
+        item.id = "startTranslucency";
+        item.innerText = "Start Translucency";
+        item.onclick =  function () {
+            _this.OnStartTranslucencyClicked();
+        }
+        itemGroup.appendChild(item);
+    }
+    else {
+        item = document.createElement("li");
+        item.id = "stopTranslucency";
+        item.innerText = "Stop Translucency";
+        item.onclick =  function () {
+            _this.OnStopTranslucencyClicked();
+        }
+        itemGroup.appendChild(item);
+    }
+    contextMenuDiv.appendChild(itemGroup);
+
+    contextMenuDiv.style.top = y + "px";
+    contextMenuDiv.style.left = x + "px";
+    contextMenuDiv.style.display = "block";
+
+    // close context menu, if mouse click is done outside
+    document.onclick = function (e) {
+        if (e.target.id !== 'contextMenu') {            
+            contextMenuDiv.style.display = 'none';
+        }
+    };
+   
+    //document.body.appendChild(contextMenuDiv);
+    // var i = document.getElementById("contextMenu").style;
+    // i.top = y + "px";
+    // i.left = x + "px";
+    // i.visibility = "visible";
+    // i.opacity = "1";
+}
+
 CheckViewerContextMenu.prototype.Init = function () {
 
     var _this = this;
-    // this.ModelBrowser = modelBrowser;
-    container = "#" + this.WebViewer._params.containerId;
+    this.WebViewer.setCallbacks({
+        contextMenu: function (position) {
+            // if (currentViewer === undefined) {
+            //     currentViewer = viewer;
+            // }
 
-    $(container).contextMenu({
-        className: 'contextMenu_style',
-        selector: 'div',
-        // selector: '.jsgrid-row, .jsgrid-alt-row',
-        build: function ($triggerElement, e) {
-            return {
-                callback: function (key, options) {
-                    _this.OnMenuItemClicked(key, options);
-                },
-                items: {
-                    "hide": {
-                        name: "Hide"
-                    },
-                    "isolate": {
-                        name: "Isolate"
-                    },
-                    "showAll": {
-                        name: "Show All"
-                    },
-                    "showNavigationCube": {
-                        name: "Show Navigation Cube"
-                    },
-                    "hideNavigationCube": {
-                        name: "Hide Navigation Cube"
-                    },
-                    "startExplode": {
-                        name: "Start Explode"
-                    },
-                    "stopExplode": {
-                        name: "Stop Explode"
-                    },
-                    "startTranslucency": {
-                        name: "Start Translucency"
-                    },
-                    "stopTranslucency": {
-                        name: "Stop Translucency"
-                    },                   
-                }
-            };
+            _this.ShowMenu(event.clientX, event.clientY);
         }
     });
+
+    /////////////////
+    // var viewerContainer = document.getElementById(this.WebViewer._params.containerId);    
+    // var _this = this;
+    // // this.ModelBrowser = modelBrowser;
+    // container = "#" + this.WebViewer._params.containerId;
+
+    // $(container).contextMenu({
+    //     className: 'contextMenu_style',
+    //     selector: 'div',
+    //     // selector: '.jsgrid-row, .jsgrid-alt-row',
+    //     build: function ($triggerElement, e) {
+    //         return {
+    //             callback: function (key, options) {
+    //                 _this.OnMenuItemClicked(key, options);
+    //             },
+    //             items: {
+    //                 "hide": {
+    //                     name: "Hide"
+    //                 },
+    //                 "isolate": {
+    //                     name: "Isolate"
+    //                 },
+    //                 "showAll": {
+    //                     name: "Show All"
+    //                 },
+    //                 "showNavigationCube": {
+    //                     name: "Show Navigation Cube"
+    //                 },
+    //                 "hideNavigationCube": {
+    //                     name: "Hide Navigation Cube"
+    //                 },
+    //                 "startExplode": {
+    //                     name: "Start Explode"
+    //                 },
+    //                 "stopExplode": {
+    //                     name: "Stop Explode"
+    //                 },
+    //                 "startTranslucency": {
+    //                     name: "Start Translucency"
+    //                 },
+    //                 "stopTranslucency": {
+    //                     name: "Stop Translucency"
+    //                 },                   
+    //             }
+    //         };
+    //     }
+    // });
 }
 
-CheckViewerContextMenu.prototype.OnMenuItemClicked = function (key, options) {
-    if (key.toLowerCase() === "hide") {
-        this.OnHideClicked();
-    }
-    else if (key.toLowerCase() === "shownavigationcube") {
-        showNavigationCube(this.WebViewer);
-    }
-    else if (key.toLowerCase() === "hidenavigationcube") {
-        hideNavigationCube(this.WebViewer);
-    }
-    else if (key.toLowerCase() === "startexplode") {
-        this.OnStartExplodeClicked();
-    }
-    else if (key.toLowerCase() === "stopexplode") {
-        this.OnStopExplodeClicked();
-    }
-    else if (key.toLowerCase() === "isolate") {
-        this.OnIsolateClicked();
-    }
-    else if (key.toLowerCase() === "showall") {
-        this.OnShowAllClicked();
-    }
-    else if (key.toLowerCase() === "starttranslucency") {
-        this.OnStartTranslucencyClicked();
-    }
-    else if (key.toLowerCase() === "stoptranslucency") {
-        this.OnStopTranslucencyClicked();
-    }
-}
+// CheckViewerContextMenu.prototype.OnMenuItemClicked = function (key, options) {
+//     if (key.toLowerCase() === "hide") {
+//         this.OnHideClicked();
+//     }
+//     else if (key.toLowerCase() === "shownavigationcube") {
+//         showNavigationCube(this.WebViewer);
+//     }
+//     else if (key.toLowerCase() === "hidenavigationcube") {
+//         hideNavigationCube(this.WebViewer);
+//     }
+//     else if (key.toLowerCase() === "startexplode") {
+//         this.OnStartExplodeClicked();
+//     }
+//     else if (key.toLowerCase() === "stopexplode") {
+//         this.OnStopExplodeClicked();
+//     }
+//     else if (key.toLowerCase() === "isolate") {
+//         this.OnIsolateClicked();
+//     }
+//     else if (key.toLowerCase() === "showall") {
+//         this.OnShowAllClicked();
+//     }
+//     else if (key.toLowerCase() === "starttranslucency") {
+//         this.OnStartTranslucencyClicked();
+//     }
+//     else if (key.toLowerCase() === "stoptranslucency") {
+//         this.OnStopTranslucencyClicked();
+//     }
+// }
 
 CheckViewerContextMenu.prototype.OnStartExplodeClicked = function () {
-    if (!this.WebViewer ) {
+    if (!this.WebViewer) {
         return;
     }
 
@@ -122,8 +274,7 @@ CheckViewerContextMenu.prototype.OnStopExplodeClicked = function () {
     this.ExplodeManager = undefined;
 }
 
-CheckViewerContextMenu.prototype.OnHideClicked = function()
-{
+CheckViewerContextMenu.prototype.OnHideClicked = function () {
     if (this.WebViewer) {
 
         var results = this.WebViewer.selectionManager.getResults();
@@ -131,7 +282,7 @@ CheckViewerContextMenu.prototype.OnHideClicked = function()
         var map = {};
         for (var i = 0; i < results.length; i++) {
             var selectedItem = results[i];
-            map[selectedItem._nodeId] = false;           
+            map[selectedItem._nodeId] = false;
         }
 
         this.WebViewer.model.setNodesVisibilities(map);
@@ -167,8 +318,8 @@ CheckViewerContextMenu.prototype.OnShowAllClicked = function () {
 
     var _this = this;
     this.WebViewer.model.setNodesVisibility([this.WebViewer.model.getAbsoluteRootNode()], true).then(function () {
-        _this.WebViewer.view.fitWorld();        
-    });   
+        _this.WebViewer.view.fitWorld();
+    });
 }
 
 
