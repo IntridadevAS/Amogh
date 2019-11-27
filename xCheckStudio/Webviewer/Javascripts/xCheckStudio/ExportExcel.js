7514d719f43aa8cbd5b52f69875ed7149fe8153e
Pooja Galande <pooja.galande@prototechsolutions.com>
2019-11-27 14:47:14 +0530

    Update in expot Excel

--- xCheckStudio/Webviewer/Javascripts/xCheckStudio/ExportExcel.js
+++ xCheckStudio/Webviewer/Javascripts/xCheckStudio/ExportExcel.js
@@ -10,14 +10,14 @@ ExportExcel.prototype.ExportComparisonComponents = function (selectedTables, exp
     var _this = this;
     return new Promise((resolve) => {
         if (exportProperties) {
-            this.CreateDummyDataGrids(selectedTables, exportProperties).then(function () {
-                _this.ExportGroups(selectedTables, exportProperties);
+            this.CreateDummyDataGrids(selectedTables).then(function () {
+                _this.ExportGroups(selectedTables);
                 return resolve(true);
             });
         }
         else {
             this.CreateComponentDataGrid(selectedTables).then(function () {
-                _this.ExportGroups(selectedTables, exportProperties);
+                _this.ExportGroups(selectedTables);
                 return resolve(true);
             });
         }
@@ -62,43 +62,6 @@ ExportExcel.prototype.CreateComponentDataGrid = function (selectedTables) {
 }
 
 ExportExcel.prototype.CreateDummyDataGrids = async function (selectedTables) {
-    // var _this = this;
-    // var header = [{ "caption": checkResults.sourceInfo.sourceAFileName, "dataField": "SourceAName" },
-    // { "caption": checkResults.sourceInfo.sourceBFileName, "dataField": "SourceBName" },
-    // { "caption": "Status", "dataField": "Status" },
-    // { "caption": "Information", "dataField": "Information" }];
-
-    // for (var id = 0; id < selectedTables.length; id++) {
-    //     var tableName = selectedTables[id];
-
-    //     var components = this.GetCategoryComponents(tableName);
-
-    //     for (var componentId in components) {
-    //         var component = components[componentId];
-
-    //         var data = [{ "SourceAName": component.sourceAName, "SourceBName": component.sourceBName, "Status": component.status, "Information": "" }];
-
-    //         var parentTable = document.getElementById("comparisonTables");
-    //         var datagridDiv = document.createElement("DIV");
-    //         datagridDiv.id = component.id + "_tempTable";
-    //         parentTable.append(datagridDiv);
-
-    //         await _this.CreateTemporaryComponentGrid(data, datagridDiv.id, header, component.id);
-
-    //         var propertyDiv = document.createElement("DIV");
-    //         propertyDiv.id = component.id + "_tempTable" + "_tempPropertiestable";
-    //         parentTable.append(propertyDiv);
-
-    //         var sources = checkResults["Comparisons"][0]["sources"];
-    //         var headers = _this.CreateComparisonPropertiesTableHeader(sources);
-
-    //         var properties = component["properties"];
-    //         var data = _this.CreateComparisonTableData(properties);
-
-    //         await _this.CreateTemporaryPropertiesGrid(data, propertyDiv.id, headers);
-    //     }
-    // }
-
     var _this = this;
     var comparison = new ComparisonData();
     var headers = comparison.CreateTableHeader(selectedTables);
@@ -111,6 +74,7 @@ ExportExcel.prototype.CreateDummyDataGrids = async function (selectedTables) {
         var parentTable = document.getElementById("comparisonTables");
         var datagridDiv = document.createElement("DIV");
         datagridDiv.id = tableName + "_tempTable";
+        datagridDiv.style.display = "none";
         parentTable.append(datagridDiv);
 
         await _this.CreateTemporaryComponentGrid(tableData, datagridDiv.id, header);
@@ -119,7 +83,7 @@ ExportExcel.prototype.CreateDummyDataGrids = async function (selectedTables) {
 
 }
 
-ExportExcel.prototype.ExportGroups = async function (selectedTables, exportProperties) {
+ExportExcel.prototype.ExportGroups = async function (selectedTables) {
     var _this = this;
     var worksheet = this.reviewWorkbook.addWorksheet('Comparison');
     var isLastTable = false;
@@ -134,54 +98,67 @@ ExportExcel.prototype.ExportGroups = async function (selectedTables, exportPrope
             isLastTable = true;
         }
 
-        await this.ExportGroup(tableName, isLastTable, worksheet, exportProperties).then(function (result) {
+        await this.ExportGroup(tableName, isLastTable, worksheet).then(function (result) {
             if (result) {
                 _this.rowCount = result;
             }
 
             if (isLastTable) {
-                _this.RemoveTempTables(selectedTables, exportProperties);
+                _this.RemoveTempTables(selectedTables);
             }
         });
 
     }
 }
 
-ExportExcel.prototype.RemoveTempTables = function (selectedTables, exportProperties) {
+ExportExcel.prototype.RemoveTempTables = function (selectedTables) {
     for (var id = 0; id < selectedTables.length; id++) {
         var tableName = selectedTables[id];
 
-        var components = this.GetCategoryComponents(tableName);
-
-        if (exportProperties) {
-            for (var componentId in components) {
-                var component = components[componentId];
-                $("#" + component.id + "_tempTable").remove();
-                $("#" + component.id + "_tempTable_tempPropertiestable").remove();
-            }
-        }
-        else {
-            $("#" + tableName + "_tempTable").remove();
-        }
+        $("#" + tableName + "_tempTable").remove();
 
     }
 }
 
-ExportExcel.prototype.ExportGroup = async function (tableName, isLastTable, worksheet, exportProperties) {
+ExportExcel.prototype.ExportGroup = async function (tableName, isLastTable, worksheet) {
     var _this = this;
-    // var components = this.GetCategoryComponents(tableName);
-    var save = false;
 
-    worksheet.mergeCells(this.rowCount, 1, this.rowCount, 2);
-    worksheet.mergeCells(this.rowCount, 3, this.rowCount, 4);
-    Object.assign(worksheet.getRow(this.rowCount).getCell(1), { value: tableName, font: { bold: true, size: 16, underline: 'double' } });
+    Object.assign(worksheet.getRow(this.rowCount).getCell(1), { value: tableName, font: { bold: true, size: 12 }, alignment: { horizontal: 'left', vertical: 'top'}, });
 
     var dataGrid = $("#" + tableName + "_tempTable").dxDataGrid(("instance"));
+
+    var headerStyles = {
+        font: { bold: true, size: 11 },
+        alignment: { horizontal: 'left', vertical: 'top'},
+        border : {
+            top: {style:'thin', color: {argb:'000000'}},
+            left: {style:'thin', color: {argb:'000000'}},
+            bottom: {style:'thin', color: {argb:'000000'}},
+            right: {style:'thin', color: {argb:'000000'}}
+          }
+    }
 
     await DevExpress.excelExporter.exportDataGrid({
         worksheet: worksheet,
         component: dataGrid,
-        topLeftCell: { row: _this.rowCount, column: 5 },
+        topLeftCell: { row: _this.rowCount + 1, column: 1 },
+        customizeCell: function(options) {
+            var gridCell = options.gridCell;
+            var excelCell = options.cell;
+            excelCell.alignment = {horizontal: 'left', vertical: 'top'} 
+            excelCell.border = {
+                top: {style:'thin', color: {argb:'000000'}},
+                left: {style:'thin', color: {argb:'000000'}},
+                bottom: {style:'thin', color: {argb:'000000'}},
+                right: {style:'thin', color: {argb:'000000'}}
+                };
+
+            excelCell.width = 30;
+
+            if(gridCell.rowType == "header") {
+                excelCell.style = headerStyles;
+            }
+        },
     }).then(function (dataGridRange) {
         if (isLastTable) {
             _this.ComparisonSheetExported = true;
@@ -193,82 +170,7 @@ ExportExcel.prototype.ExportGroup = async function (tableName, isLastTable, work
     });
 }
 
-ExportExcel.prototype.ExportComponent = function (component, worksheet, save, exportProperties) {
-    var _this = this;
-    return new Promise((resolve) => {
-        var dataGrid = $("#" + component.id + "_tempTable").dxDataGrid(("instance"));
-        DevExpress.excelExporter.exportDataGrid({
-            worksheet: worksheet,
-            component: dataGrid,
-            topLeftCell: { row: _this.rowCount, column: 5 },
-        }).then(function (dataGridRange) {
-            if (exportProperties) {
-                _this.rowCount = dataGridRange.to.row + 2;
-
-                var dataGrid1 = $("#" + component.id + "_tempTable" + "_tempPropertiestable").dxDataGrid("instance");
-
-                DevExpress.excelExporter.exportDataGrid({
-                    worksheet: worksheet,
-                    component: dataGrid1,
-                    topLeftCell: { row: _this.rowCount, column: 5 },
-                }).then(function (dataGridRange) {
-                    if (save) {
-                        _this.ComparisonSheetExported = true;
-                        _this.SaveWorkBook();
-                    }
-                    _this.rowCount = dataGridRange.to.row + 3;
-                    return resolve(_this.rowCount);
-                });
-            }
-            else {
-                if (save) {
-                    _this.ComparisonSheetExported = true;
-                    _this.SaveWorkBook();
-                }
-                _this.rowCount = dataGridRange.to.row + 1;
-                return resolve(_this.rowCount);
-            }
-        });
-    });
-}
-
 ExportExcel.prototype.CreateTemporaryComponentGrid = function (component, containerDiv, header) {
-    return new Promise((resolve) => {
-        $(function () {
-            $("#" + containerDiv).dxDataGrid({
-                dataSource: component,
-                columns: header,
-                columnAutoWidth: true,
-                width: "0px",
-                height: "0px",
-                wordWrapEnabled: false,
-                showBorders: true,
-                showRowLines: true,
-                allowColumnResizing: true,
-                hoverStateEnabled: true,
-                filterRow: {
-                    visible: true
-                },
-                selection: {
-                    mode: "multiple",
-                    showCheckBoxesMode: "always",
-                    recursive: true
-                },
-                scrolling: {
-                    mode: "standard"
-                },
-                paging: {
-                    enabled: false
-                },
-                onContentReady: function (e) {
-                    return resolve(true);
-                },
-            });
-        });
-    });
-}
-
-ExportExcel.prototype.CreateTemporaryPropertiesGrid = function (component, containerDiv, header) {
     return new Promise((resolve) => {
         $(function () {
             $("#" + containerDiv).dxDataGrid({
@@ -338,64 +240,6 @@ ExportExcel.prototype.SaveWorkBook = function() {
     // }
 }
 
-ExportExcel.prototype.CreateComparisonTableData = function (properties) {
-
-    var property;
-    var tableData = [];
-    for (var propertyId in properties) {
-        property = properties[propertyId];
-        tableRowContent = {};
-        tableRowContent[ComparisonPropertyColumnNames.SourceAName] = property.sourceAName;
-        tableRowContent[ComparisonPropertyColumnNames.SourceAValue] = property.sourceAValue;
-        tableRowContent[ComparisonPropertyColumnNames.SourceBValue] = property.sourceBValue;
-        tableRowContent[ComparisonPropertyColumnNames.SourceBName] = property.sourceBName;
-        tableRowContent[ComparisonPropertyColumnNames.SourceCName] = property.sourceCName;
-        tableRowContent[ComparisonPropertyColumnNames.SourceCValue] = property.sourceCValue;  
-        tableRowContent[ComparisonPropertyColumnNames.SourceDValue] = property.sourceDValue;
-        tableRowContent[ComparisonPropertyColumnNames.SourceDName] = property.sourceDName;
-
-        tableRowContent[ComparisonPropertyColumnNames.Status] = property.severity;
-
-        if(property.sourceAName == null) {
-            tableRowContent[ComparisonPropertyColumnNames.SourceAName] = "";
-        }
-        if(property.sourceAValue == null) {
-            tableRowContent[ComparisonPropertyColumnNames.SourceAValue] = "";
-        }
-        if(property.sourceBName == null) {
-            tableRowContent[ComparisonPropertyColumnNames.SourceBName] = "";
-        }
-        if(property.sourceBValue == null) {
-            tableRowContent[ComparisonPropertyColumnNames.SourceBValue] = "";
-        }
-        if(property.sourceCName == null) {
-            tableRowContent[ComparisonPropertyColumnNames.SourceCName] = "";
-        }
-        if(property.sourceCValue == null) {
-            tableRowContent[ComparisonPropertyColumnNames.SourceCValue] = "";
-        }
-        if(property.sourceDName == null) {
-            tableRowContent[ComparisonPropertyColumnNames.SourceDName] = "";
-        }
-        if(property.sourceDValue == null) {
-            tableRowContent[ComparisonPropertyColumnNames.SourceDValue] = "";
-        }
-
-        if (property.transpose == 'lefttoright' && property.severity !== 'No Value') {
-            tableRowContent[ComparisonPropertyColumnNames.Status] = 'OK(T)';
-            tableRowContent[ComparisonPropertyColumnNames.SourceBValue] = property.sourceAValue;
-        }
-        else if (property.transpose == 'righttoleft' && property.severity !== 'No Value') {
-            tableRowContent[ComparisonPropertyColumnNames.Status] = 'OK(T)';
-            tableRowContent[ComparisonPropertyColumnNames.SourceAValue] = property.sourceBValue;
-        }
-
-        tableData.push(tableRowContent);
-    }
-
-    return tableData;
-}
-
 function ComparisonData() {
     this.ComparisonExportComplete = false;
 }
@@ -444,7 +288,7 @@ ComparisonData.prototype.CreateTableData = function(selectedTables) {
                     property1["caption"] = property.sourceAName;
 
                     if(property.sourceAName == null) {
-                        property1["caption"] = "";
+                        property1["caption"] = "Missing property";
                     }
 
                     propName.push(property1["caption"]);
@@ -461,7 +305,7 @@ ComparisonData.prototype.CreateTableData = function(selectedTables) {
                     property2["caption"] = property.sourceBName;
 
                     if(property.sourceBName == null) {
-                        property2["caption"] = "";
+                        property2["caption"] = "Missing property";
                     }
 
                     propertyObj["propertB_"] = property.sourceBValue;
@@ -474,37 +318,42 @@ ComparisonData.prototype.CreateTableData = function(selectedTables) {
 
                 }
                 
-                // if(sources.length > 2) {
+                if(sources.length > 2) {
 
-                //     var property3 = {};
-                //     property3["caption"] = property.sourceCName;
+                    var property3 = {};
+                    property3["caption"] = property.sourceCName;
 
-                //     if(property.sourceCName == null) {
-                //         property3["caption"] = "";
-                //     }
+                    if(property.sourceCName == null) {
+                        property3["caption"] = "Missing property";
+                    }
 
-                //     propName.push(property3["caption"]);
+                    propertyObj["propertC_"] = property.sourceCValue;
 
-                //     property3["dataField"] = "propert_C" + propertyIndex;
+                    if(property.sourceBValue == null) {
+                        propertyObj["propertC_"] = "";
+                    }
 
-                //     group.push(property3);
 
-                // }
+                    propName.push(property3["caption"]);
 
-                // if(sources.length > 3) {
-                //     var property4 = {};
-                //     property4["caption"] = property.sourceDName;
+                }
 
-                //     if(property.sourceDName == null) {
-                //         property4["caption"] = "";
-                //     }
+                if(sources.length > 3) {
+                    var property4 = {};
+                    property4["caption"] = property.sourceDName;
 
-                //     propName.push(property4["caption"]);
+                    if(property.sourceDName == null) {
+                        property4["caption"] = "Missing property";
+                    }
 
-                //     property4["dataField"] = "propert_D" + propertyIndex;
+                    propertyObj["propertD_"] = property.sourceDValue;
 
-                //     group.push(property4);
-                // }
+                    if(property.sourceDValue == null) {
+                        propertyObj["propertD_"] = "";
+                    }
+
+                    propName.push(property4["caption"]);
+                }
 
                 var a = JSON.stringify(sourcePropertyNamesGroup);
                 var b = JSON.stringify(propName);
@@ -601,7 +450,7 @@ ComparisonData.prototype.CreateTableHeader = function(selectedTables) {
                     property1["caption"] = property.sourceAName;
 
                     if(property.sourceAName == null) {
-                        property1["caption"] = "";
+                        property1["caption"] = "Missing property";
                     }
 
                     propName.push(property1["caption"]);
@@ -614,7 +463,7 @@ ComparisonData.prototype.CreateTableHeader = function(selectedTables) {
                     property2["caption"] = property.sourceBName;
 
                     if(property.sourceBName == null) {
-                        property2["caption"] = "";
+                        property2["caption"] = "Missing property";
                     }
 
                     propName.push(property2["caption"]);
@@ -631,7 +480,7 @@ ComparisonData.prototype.CreateTableHeader = function(selectedTables) {
                     property3["caption"] = property.sourceCName;
 
                     if(property.sourceCName == null) {
-                        property3["caption"] = "";
+                        property3["caption"] = "Missing property";
                     }
 
                     propName.push(property3["caption"]);
@@ -647,7 +496,7 @@ ComparisonData.prototype.CreateTableHeader = function(selectedTables) {
                     property4["caption"] = property.sourceDName;
 
                     if(property.sourceDName == null) {
-                        property4["caption"] = "";
+                        property4["caption"] = "Missing property";
                     }
 
                     propName.push(property4["caption"]);
